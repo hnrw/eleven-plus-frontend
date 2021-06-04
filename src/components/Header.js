f6b047871fdf1d3532abbdc2f40a3f91b21c677d
Henry <henry@henrywu.co.uk>
2021-06-04 20:01:48 +0100

    use pandarain header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,69 +1,64 @@
 import React from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { AppBar, Toolbar, Typography, Button, Divider } from "@material-ui/core"
+import { useDispatch, useSelector } from "react-redux"
 import { Link, useHistory } from "react-router-dom"
+import { AppBar, Toolbar, Typography, Button, Divider } from "@material-ui/core"
+import toast from "react-hot-toast"
+
 import { clearUser } from "../reducers/userReducer"
 
 const Header = () => {
-  const dispatch = useDispatch()
   const history = useHistory()
+  const dispatch = useDispatch()
   const user = useSelector((state) => state.user)
 
   const handleLogout = (event) => {
     event.preventDefault()
     dispatch(clearUser())
-    dispatch(clearCredits())
-    window.sessionStorage.removeItem("savedQuestion")
+    history.push("/login")
   }
 
-  const mobile = window.innerWidth <= 500
-
-  const loggedIn = () => (
-    <>
-      {window.location.host === "localhost:3000" && (
-        <Link
-          to="/request-questions"
-          style={{ textDecoration: "none", color: "#000" }}
-        >
-          <Button>RequestQuestion</Button>
-        </Link>
-      )}
-      <Link
-        to={`/${user.username}`}
-        style={{ textDecoration: "none", color: "#000" }}
-      >
-        <Button>{user.username}</Button>
+  const HeaderItem = (props) => {
+    return (
+      <Link to={props.link} style={{ textDecoration: "none", color: "black" }}>
+        <Button>{props.children}</Button>
       </Link>
-      <Link to="/account" style={{ textDecoration: "none", color: "#000" }}>
-        <Button>Account</Button>
-      </Link>
-      <Button onClick={handleLogout}>Logout</Button>
-    </>
-  )
+    )
+  }
 
   const loggedOut = () => (
     <>
-      <Button onClick={() => history.push("/login")}>Log in</Button>
-      <Button onClick={() => history.push("/signup")}>Sign up</Button>
+      <HeaderItem link="login">Log in</HeaderItem>
+      <HeaderItem link="signup">Sign up</HeaderItem>
+    </>
+  )
+
+  const loggedIn = () => (
+    <>
+      {user.isTeacher && <HeaderItem link="students">Students</HeaderItem>}
+      {!user.isTeacher && (
+        <HeaderItem link="/flashcard/f">Flashcards</HeaderItem>
+      )}
+      {user.email === "henry@henrywu.co.uk" && (
+        <HeaderItem link="/admin">Admin</HeaderItem>
+      )}
+      <Button onClick={handleLogout}>Log out</Button>
     </>
   )
 
   return (
     <>
+      <span>
+        {user?.email} {user?.name}
+      </span>
       <AppBar position="fixed" style={{ margin: 0, backgroundColor: "white" }}>
         <Toolbar>
           <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-            <Typography
-              variant="h6"
-              noWrap
-              style={{ fontSize: mobile ? 16 : null }}
-            >
-              Backstage
+            <Typography variant="h6" noWrap>
+              Waterfront
             </Typography>
           </Link>
+          {/* somehow sets to the right of the app bar marginRight not needed here, but could play with positioning */}
           <section style={{ marginLeft: "auto", marginRight: 0 }}>
-            {/* somehow sets to the right of the app bar
-  marginRight not needed here, but could play with positioning */}
             {user ? loggedIn() : loggedOut()}
           </section>
         </Toolbar>
