b6724cc40ec186d7717090f2c5ae4472e9a85563
Henry <henry@henrywu.co.uk>
2021-06-05 19:39:09 +0100

    add percentile text

--- src/components/stats/Stats.js
+++ src/components/stats/Stats.js
@@ -6,13 +6,18 @@ import Radar from "./Radar"
 import HalfPie from "./HalfPie"
 import Percentile from "./Percentile"
 
-
 const Stats = () => {
   return (
     <>
       <Radar />
       <Percentile />
       <HalfPie />
+      <Container maxWidth="sm">
+        <Typography>
+          Only 1 in 10 children who take the 11+ exam get into grammar schools.
+          You should aim to be in the blue section - the top 90%
+        </Typography>
+      </Container>
     </>
   )
 }
