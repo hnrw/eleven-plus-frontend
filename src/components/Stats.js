1d349e1856c3246849dd12685dbf47398c9a512d
Henry <henry@henrywu.co.uk>
2021-06-05 10:52:44 +0100

    create radar graph for stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { TextField, Typography, Container, Button } from "@material-ui/core"
+import { ResponsiveRadar } from "@nivo/radar"
 
 const Stats = () => {
   const percentile = 90
@@ -21,11 +22,64 @@ const Stats = () => {
       skill: "Units",
       rating: 60,
     },
+    {
+      skill: "Test",
+      rating: 90,
+    },
   ]
+
   return (
     <>
-      <Typography>This is line one</Typography>
-      <Button>This is a button</Button>
+      <div style={{ height: 500, fontFamily: "Roboto" }}>
+        <ResponsiveRadar
+          data={radar}
+          keys={["rating"]}
+          indexBy="skill"
+          maxValue={100}
+          margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+          curve="linearClosed"
+          borderWidth={2}
+          borderColor={{ from: "color" }}
+          gridLevels={5}
+          gridShape="linear"
+          gridLabelOffset={36}
+          enableDots={true}
+          dotSize={10}
+          dotColor={{ theme: "background" }}
+          dotBorderWidth={2}
+          dotBorderColor={{ from: "color" }}
+          enableDotLabel={true}
+          dotLabel="value"
+          dotLabelYOffset={-12}
+          colors={{ scheme: "nivo" }}
+          fillOpacity={0.25}
+          blendMode="multiply"
+          animate={true}
+          motionConfig="wobbly"
+          isInteractive={true}
+          legends={[
+            {
+              anchor: "top-left",
+              direction: "column",
+              translateX: -50,
+              translateY: -40,
+              itemWidth: 80,
+              itemHeight: 20,
+              itemTextColor: "#999",
+              symbolSize: 12,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
     </>
   )
 }
