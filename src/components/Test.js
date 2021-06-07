d32a5120ff58311950f9959bf0c35f3d278048e1
Henry <henry@henrywu.co.uk>
2021-06-07 15:13:13 +0100

    submit test logic

--- src/components/Test.js
+++ src/components/Test.js
@@ -16,15 +16,18 @@ import {
 } from "react-router-dom"
 import _ from "lodash"
 import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-hot-toast"
 
 import Problem from "./Problem"
 import testService from "../services/testService"
+import answerService from "../services/answerService"
 import { setTest } from "../reducers/testReducer"
 
 const Test = ({ manualTest }) => {
   const dispatch = useDispatch()
   const history = useHistory()
   const test = useSelector((state) => state.test)
+  const user = useSelector((state) => state.user)
 
   useEffect(() => {
     if (!manualTest) {
@@ -35,6 +38,34 @@ const Test = ({ manualTest }) => {
   const renderedTest = manualTest || test
   console.log(renderedTest)
 
+  const handleSubmit = async () => {
+    // try {
+    //   await testproblems.reduce(async (memo, problem) => {
+    //     await memo
+    //     await submitAnswer(problem)
+    //   }, undefined)
+    //   toast.success("done")
+    // } catch (err) {
+    //   toast.error("master err")
+    // }
+    await test.problems.reduce(async (memo, problem) => {
+      await memo
+
+      await submitAnswer(problem)
+    }, undefined)
+  }
+
+  const submitAnswer = async (problem) => {
+    const { id, selected } = problem
+
+    answerService.submitAnswer({ selected, problemId: id, token: user.token })
+    // try {
+    //   answerService.submitAnswer({ selected, problemId: id })
+    // } catch (err) {
+    //   toast.error("error")
+    // }
+  }
+
   if (!renderedTest) return null
 
   return (
@@ -43,7 +74,7 @@ const Test = ({ manualTest }) => {
         <Problem key={problem.question} problem={problem} />
       ))}
       <div>
-        <Button onClick={() => history.push("/results")}>Submit</Button>
+        <Button onClick={handleSubmit}>Submit</Button>
       </div>
     </Container>
   )
