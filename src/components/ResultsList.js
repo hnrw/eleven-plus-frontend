5f1751d5b4e7bb8710e546027ea55fb370305a50
Henry <henry@henrywu.co.uk>
2021-06-07 20:21:37 +0100

    format

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -2,12 +2,21 @@ import React, { useEffect, useState } from "react"
 import { TextField, Typography, Container, Button } from "@material-ui/core"
 import gradedTestService from "../services/gradedTestService"
 import { useDispatch, useSelector } from "react-redux"
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Link,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom"
 
 const ResultsList = () => {
   const [gradedTestsList, setGradedTestsList] = useState([])
   const user = useSelector((state) => state.user)
 
   useEffect(() => {
+    console.log(user)
     if (user) {
       gradedTestService
         .fetchTests(user.token)
@@ -18,7 +27,12 @@ const ResultsList = () => {
   return (
     <>
       {gradedTestsList.map((gt) => (
-        <div>{gt.id}</div>
+        <Link
+          to={`/results/${gt.id}`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <Button>{gt.id}</Button>
+        </Link>
       ))}
     </>
   )
