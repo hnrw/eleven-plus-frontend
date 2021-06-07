f571cd5ee509781a0c3a04bf2e6351a21738d706
Henry <henry@henrywu.co.uk>
2021-06-08 00:03:32 +0100

    pie chart mobile

--- src/components/stats/HalfPie.js
+++ src/components/stats/HalfPie.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react"
 import { ResponsivePie } from "@nivo/pie"
 import {
   TextField,
@@ -46,60 +47,69 @@ const margin = { top: 40, right: 80, bottom: 80, left: 80 }
 const angle = (percent) => {
   // pointer is positioned slightly above the bottom of the chart
   // so needs slightly larger than 180deg to cover full arc
-
-  // pointer is slightly unsymmetrical, so needs a bit extra to reach 100%
+  // also pointer is slightly unsymmetrical, so needs a bit extra to reach 100%
   return -95 + percent * 1.91
 }
 
-const HalfPie = () => (
-  <div
-    style={{
-      height: 450,
-      position: "relative",
-    }}
-  >
-    <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-      innerRadius={0.7}
-      padAngle={0.7}
-      startAngle={-90}
-      endAngle={90}
-      cornerRadius={3}
-      activeOuterRadiusOffset={8}
-      borderWidth={1}
-      borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-      arcLabel="id"
-      arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
-      enableArcLinkLabels={false}
-      colors={{ datum: "data.color" }}
-      theme={{ fontSize: 14 }}
-      isInteractive={false}
-    />
-    <span
+const HalfPie = () => {
+  const [mobile, setMobile] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth < 500) {
+      setMobile(true)
+    }
+  }, [])
+
+  return (
+    <div
       style={{
-        position: "absolute",
-        // top: 100,
-        right: margin.right,
-        bottom: -140,
-        left: margin.left,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        pointerEvents: "none",
+        height: mobile ? 250 : 450,
+        position: "relative",
       }}
     >
-      <img
-        src={pointer}
-        style={{
-          height: 500,
-          transform: "rotate(20deg)",
-          transform: `rotate(${angle(83)}deg)`,
-        }}
+      <ResponsivePie
+        data={data}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.7}
+        padAngle={0.7}
+        startAngle={-90}
+        endAngle={90}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        borderWidth={1}
+        borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+        arcLabel="id"
+        arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+        enableArcLinkLabels={false}
+        colors={{ datum: "data.color" }}
+        theme={{ fontSize: 14 }}
+        isInteractive={false}
       />
-    </span>
-  </div>
-)
+      <span
+        style={{
+          position: "absolute",
+          // top: 100,
+          right: margin.right,
+          bottom: mobile ? 5 : -140,
+          left: margin.left,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          pointerEvents: "none",
+        }}
+      >
+        <img
+          src={pointer}
+          style={{
+            height: mobile ? 180 : 500,
+            transform: "rotate(20deg)",
+            transform: `rotate(${angle(83)}deg)`,
+          }}
+        />
+      </span>
+    </div>
+  )
+}
 
 export default HalfPie
