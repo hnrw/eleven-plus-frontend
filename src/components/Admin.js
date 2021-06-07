550fd7da4b5f2842bb590dbd73fc05c4cb54a06a
Henry <henry@henrywu.co.uk>
2021-06-07 11:51:24 +0100

    format test list

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react"
-import { TextField, Typography, Container, Button } from "@material-ui/core"
+import {
+  Divider,
+  TextField,
+  Typography,
+  Container,
+  Button,
+} from "@material-ui/core"
 import { useDispatch, useSelector } from "react-redux"
 import {
   BrowserRouter as Router,
@@ -16,14 +22,13 @@ import testService from "../services/testService"
 import { setTest } from "../reducers/testReducer"
 
 const styles = {
-  testTitle: {
-    textDecoration: "underlined",
-    cursor: "pointer",
-  },
+  testTitle: {},
   testDiv: {
     marginTop: 20,
     marginBottom: 20,
+    cursor: "pointer",
   },
+  divider: { marginTop: 20 },
 }
 
 const Admin = () => {
@@ -48,17 +53,17 @@ const Admin = () => {
       </Button>
       {tests &&
         tests.map((t) => (
-          <div key={t.id} style={styles.testDiv}>
-            <Typography
-              variant="h5"
-              style={styles.testTitle}
-              onClick={() => selectTest(t.id)}
-            >
+          <div
+            onClick={() => selectTest(t.id)}
+            key={t.id}
+            style={styles.testDiv}
+          >
+            <Typography variant="h5" style={styles.testTitle}>
               Test {t.num}
             </Typography>
             <Typography>{t.problems.length} questions</Typography>
-
             {t.open && t.problems.map((p) => <Problem problem={p} viewOnly />)}
+            <Divider style={styles.divider} />
           </div>
         ))}
     </Container>
