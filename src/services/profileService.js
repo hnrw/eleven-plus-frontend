8f699537f8fc6cdd62b11c7fadd31a868a33face
Henry <henry@henrywu.co.uk>
2021-06-11 13:54:34 +0100

    quick hack with comment

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = process.env.REACT_APP_API
-const url = `api/profile`
+const url = `${api}/profile`
 
 const fetchProfiles = async () => {
   const response = await axios.get(url)
@@ -18,6 +18,8 @@ const getProfile = async (token) => {
   return response.data
 }
 
+// in the future, allow users controller to update any info and check it's from admin
+// then create profile router that only allows editing profile info
 const updateProfile = async (data) => {
   const { token, profileData } = data
   const config = {
@@ -25,7 +27,7 @@ const updateProfile = async (data) => {
       Authorization: `Bearer ${token}`,
     },
   }
-  const response = await axios.put(url, profileData, config)
+  const response = await axios.put(`${api}/users`, profileData, config)
   return response.data
 }
 
