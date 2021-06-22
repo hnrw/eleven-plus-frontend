4e40ba9913f66f7c0d3ec5e1904ee0893fff1e6f
Henry <henry@henrywu.co.uk>
2021-06-22 22:34:21 +0100

    created gradedCategory service

---
+++ src/services/gradedCategoryService.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const baseUrl = process.env.REACT_APP_API
+const url = `${baseUrl}/graded-categories`
+
+const getGradedCategories = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.get(url)
+  return response.data
+}
+
+export default {
+  getGradedCategories,
+}
