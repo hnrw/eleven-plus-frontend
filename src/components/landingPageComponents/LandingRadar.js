e3f67551bbb98d878c7d86b707230ae9019fa7b5
Henry <henry@henrywu.co.uk>
2021-06-23 14:21:49 +0100

    move hook to own file

--- src/components/landingPageComponents/LandingRadar.js
+++ src/components/landingPageComponents/LandingRadar.js
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/core"
 
 import Radar from "../stats/Radar"
+import useInterval from "../../hooks/useInterval"
 
 const rand = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min)
@@ -63,26 +64,6 @@ const genData = () => {
   ]
 }
 
-const useInterval = (callback, delay) => {
-  const savedCallback = useRef()
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current()
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay)
-      return () => clearInterval(id)
-    }
-  }, [delay])
-}
-
 const LandingRadar = () => {
   const [radarData, setRadarData] = useState(genData)
 
@@ -98,51 +79,3 @@ const LandingRadar = () => {
 }
 
 export default LandingRadar
-
-// const radarData = [
-//   {
-//     category: "fractions",
-//     Oli: 88,
-//     Average: 78,
-//   },
-//   {
-//     category: "algebra",
-//     Oli: 86,
-//     Average: 60,
-//   },
-//   {
-//     category: "numbers",
-//     Oli: 83,
-//     Average: 61,
-//   },
-//   {
-//     category: "statistics",
-//     Oli: 74,
-//     Average: 72,
-//   },
-//   {
-//     category: "multiplication and division",
-//     Oli: 73,
-//     Average: 77,
-//   },
-//   {
-//     category: "addition and subtraction",
-//     Oli: 92,
-//     Average: 79,
-//   },
-//   {
-//     category: "measurement",
-//     Oli: 75,
-//     Average: 89,
-//   },
-//   {
-//     category: "ratio and proportion",
-//     Oli: 84,
-//     Average: 85,
-//   },
-//   {
-//     category: "geometry",
-//     Oli: 70,
-//     Average: 83,
-//   },
-// ]
