a5c4632707b9654e27f11d3526d936c62085658b
Henry <henry@henrywu.co.uk>
2021-06-23 10:09:46 +0100

    update tooltip font

--- src/components/TestResultsList.js
+++ src/components/TestResultsList.js
@@ -82,7 +82,7 @@ const TestResultsList = () => {
           <Divider style={styles.divider} />
         </Link>
       ))}
-      <div style={{ height: 400 }}>
+      <div style={{ height: 400, fontFamily: "Roboto" }}>
         <TestLineProgress data={data} />
       </div>
     </Container>
